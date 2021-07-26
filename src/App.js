1b4fe16cfa233348b60d3b2d71e9a2879e6d1514
Andrew Bentzen <bentzen.an@gmail.com>
2021-07-26 14:25:08 -0400

    fixed small compile-time error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Jumbotron from './components/Jumbotron';
 import ButtonAppBar from './components/ButtonAppBar';
-import FormPropsTextFields from './components/FormPropsTextFields';
 import ClippedDrawer from './components/ClippedDrawer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar activeLink="home" /> */}
       <ButtonAppBar />
       <ClippedDrawer />
     </div>
