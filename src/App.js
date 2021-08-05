ac360ae513516046afdcc8ae1bfa429d249ecc59
Andrew Bentzen <bentzen.an@gmail.com>
2021-08-05 13:24:23 -0400

    set up basic react-router for home and videos pages

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import FormPropsTextFields from "./components/FormPropsTextFields";
 import Fetch from "./components/Fetch";
 import Navbar from "./components/Navbar";
 import SideNav from "./components/SideNav";
+import { Switch, Route } from "react-router-dom";
 import HomeContentBlock from "./pages/HomeContentBlock";
 import VideosContentBlock from "./pages/VideosContentBlock";
 
 function App() {
   return (
     <div className="App">
-      {/* <ButtonAppBar /> */}
-      {/* <ClippedDrawer /> */}
       <Navbar />
       <SideNav />
-      <HomeContentBlock />
-      {/* <VideosContentBlock /> */}
+      <Switch>
+        <Route path="/videos">
+          <VideosContentBlock />
+        </Route>
+        <Route path="/">
+          <HomeContentBlock />
+        </Route>
+      </Switch>
     </div>
   );
 }
