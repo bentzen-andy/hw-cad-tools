38c9f91b13cc5a04688eba4b85119ad5477fe8bc
Andrew Bentzen <bentzen.an@gmail.com>
2021-08-12 20:20:37 -0400

    add some download code links and code snippets

--- src/pages/RouterComponent.js
+++ src/pages/RouterComponent.js
@@ -12,6 +12,52 @@ import Gist from "../components/Gist";
 
 const titleToURL = (title) => title.toLowerCase().replace(/ /g, "-");
 
+const tutorialRoutesList = videos.map((video) => (
+  <Route
+    key={video.id}
+    path={`/hw-cad-tools/tutorials/${titleToURL(video.title)}`}
+    exact
+  >
+    <div className="row">
+      <div className="column">
+        <SideNav sideNavTitle="Tutorials" links={videos} />
+      </div>
+      <div className="column">
+        <EmbeddedVideo
+          url={`https://www.youtube.com/embed/${video.id}`}
+          width="840"
+          height="470"
+          showTitle={true}
+        />
+      </div>
+    </div>
+  </Route>
+));
+
+const downloadsRoutesList = downloads.map((download) => (
+  <Route
+    key={download.id}
+    path={`/hw-cad-tools/downloads/${titleToURL(download.title)}`}
+    exact
+  >
+    <div className="row">
+      <div className="column">
+        <SideNav sideNavTitle="Downloads" links={downloads} />
+      </div>
+      <div className="column">
+        <div className="row">
+          <a href={download.zipDownload}>
+            <button>Download Code</button>
+          </a>
+        </div>
+        <div className="row" style={{ width: "900px" }}>
+          <Gist id={download.id}> </Gist>
+        </div>
+      </div>
+    </div>
+  </Route>
+));
+
 export default function RouterComponent() {
   return (
     <>
@@ -22,44 +68,8 @@ export default function RouterComponent() {
         <Route path="/hw-cad-tools/tutorials" exact>
           <TutorialContentBlock />
         </Route>
-
-        {videos.map((video) => (
-          <Route
-            key={video.id}
-            path={`/hw-cad-tools/tutorials/${titleToURL(video.title)}`}
-            exact
-          >
-            <div className="row">
-              <div className="column">
-                <SideNav sideNavTitle="Tutorials" links={videos} />
-              </div>
-              <div className="column">
-                <EmbeddedVideo
-                  url={`https://www.youtube.com/embed/${video.id}`}
-                  width="840"
-                  height="470"
-                  showTitle={true}
-                />
-              </div>
-            </div>
-          </Route>
-        ))}
-
-        {downloads.map((download) => (
-          <Route
-            key={download.id}
-            path={`/hw-cad-tools/downloads/${titleToURL(download.title)}`}
-            exact
-          >
-            <div className="row">
-              <div className="column">
-                <SideNav sideNavTitle="Downloads" links={videos} />
-              </div>
-              <Gist id="d30698e3a97144703848ffae2f0d354d"> </Gist>
-            </div>
-          </Route>
-        ))}
-
+        {tutorialRoutesList}
+        {downloadsRoutesList}
         <Route path="/hw-cad-tools/downloads" exact>
           <DownloadsContentBlock />
         </Route>
